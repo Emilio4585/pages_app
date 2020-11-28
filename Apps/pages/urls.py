cf483028b91193ac5e03475f5facfe34e0dfbeee
emilio4585 <emiliosalas54@gmail.com>
2020-11-28 00:45:03 -0600

    Añadi nuevas paginas

--- Apps/pages/urls.py
+++ Apps/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, AboutContactView
 
 urlpatterns = [
     path('home/', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about')
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', AboutContactView.as_view(), name='contact')
 ]
