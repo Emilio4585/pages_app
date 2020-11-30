7d25d8e737348983449e00efcfc80a6b2524d81f
emilio4585 <emiliosalas54@gmail.com>
2020-11-29 23:39:17 -0600

    add all

--- Apps/pages/tests.py
+++ Apps/pages/tests.py
@@ -12,12 +12,6 @@ class SimpleTests(SimpleTestCase):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
 
-
-class SimpleTests2(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/home')
-        self.assertEqual(response.status_code, 200)
-
-    def test_about_page_status_code(self):
-        response = self.client.get('/about')
+    def test_contact_page_status_code(self):
+        response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
